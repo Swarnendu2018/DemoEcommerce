6bb89499d4a8a69e31e56deed8519ff2d2e26031
unknown <swarnenduriku2014@gmail.com>
2025-06-28 23:45:05 +0530

    added bootstrap and navbar base

--- users/src/app/app.module.ts
+++ users/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigupLoginComponent } from './components/sigup-login/sigup-login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavbarComponent } from './components/common/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SigupLoginComponent
+    SigupLoginComponent,
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
